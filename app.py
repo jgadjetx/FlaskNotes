d6dd8d5aea8c7fe36ad0eb35e42738069e26a2c8
Phumudzo Muvhango <pa.muvhango@gmail.com>
2019-05-08 10:48:50 +0200

    Init commit

---
+++ app.py
@@ -0,0 +1,31 @@
+
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '24682468'
+app.config['MYSQL_DB'] = 'testschema'
+
+mysql = MySQL(app)
+
+
+
+@app.route("/", methods = ["GET","POST"])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(firstname, lastname) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
+
